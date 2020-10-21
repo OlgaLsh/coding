b8f0200d244b14f6f8ad028bc8d7df048865e788
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2020-10-21 16:38:31 +0300

    create project

---
+++ Distance/Distance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance
+{
+    struct Distance
+    {
+        public int A;
+        public int B;
+        public int C;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение A сначала в футах, затем в дюймах:");
+            int f = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int A = f * 12 + d;
+            Console.WriteLine("Введите значение B сначала в футах, затем в дюймах:");
+            int f1 = Convert.ToInt32(Console.ReadLine());
+            int d1 = Convert.ToInt32(Console.ReadLine());
+            int B = f1 * 12 + d1;
+            int C = A + B;
+            int C1 = C / 12;
+            int C2 = C % 12;
+            Console.WriteLine(C1 + "'" +"-" + C2 + " '' " );
+        }
+    }
+}
---
+++ Triangle/Triangle/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение периметра равностороннего треугольника:");
+            double P = Convert.ToDouble(Console.ReadLine());
+            double k = P / 3; // сторона треугольника
+            double pp = P / 2; // полупериметр
+            double S = Math.Sqrt(pp * Math.Pow((pp - k), 3)); // площадь треугольника
+            Console.WriteLine("Сторона\t\tПлощадь");
+            Console.WriteLine($"{k:.##}" + "\t\t" + $"{S:.##}");
+
+        }
+    }
+}
