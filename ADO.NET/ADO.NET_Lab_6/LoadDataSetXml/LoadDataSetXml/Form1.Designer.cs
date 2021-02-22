331aca61f299fb5f5afe70a02200181fcc33a207
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2021-02-23 00:34:26 +0300

    лабораторная 6

---
+++ ADO.NET/ADO.NET_Lab_6/LoadDataSetXml/LoadDataSetXml/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace LoadDataSetXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CustomersGrid = new System.Windows.Forms.DataGridView();
+            this.OrdersGrid = new System.Windows.Forms.DataGridView();
+            this.LoadSchemaButton = new System.Windows.Forms.Button();
+            this.LoadDataButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CustomersGrid
+            // 
+            this.CustomersGrid.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CustomersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersGrid.Location = new System.Drawing.Point(2, 1);
+            this.CustomersGrid.Name = "CustomersGrid";
+            this.CustomersGrid.RowTemplate.Height = 24;
+            this.CustomersGrid.Size = new System.Drawing.Size(286, 300);
+            this.CustomersGrid.TabIndex = 0;
+            // 
+            // OrdersGrid
+            // 
+            this.OrdersGrid.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.OrdersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrdersGrid.Location = new System.Drawing.Point(325, 1);
+            this.OrdersGrid.Name = "OrdersGrid";
+            this.OrdersGrid.RowTemplate.Height = 24;
+            this.OrdersGrid.Size = new System.Drawing.Size(285, 300);
+            this.OrdersGrid.TabIndex = 1;
+            // 
+            // LoadSchemaButton
+            // 
+            this.LoadSchemaButton.BackColor = System.Drawing.Color.Salmon;
+            this.LoadSchemaButton.Location = new System.Drawing.Point(2, 307);
+            this.LoadSchemaButton.Name = "LoadSchemaButton";
+            this.LoadSchemaButton.Size = new System.Drawing.Size(286, 42);
+            this.LoadSchemaButton.TabIndex = 2;
+            this.LoadSchemaButton.Text = "Load Schema";
+            this.LoadSchemaButton.UseVisualStyleBackColor = false;
+            this.LoadSchemaButton.Click += new System.EventHandler(this.LoadSchemaButton_Click);
+            // 
+            // LoadDataButton
+            // 
+            this.LoadDataButton.BackColor = System.Drawing.Color.Bisque;
+            this.LoadDataButton.Location = new System.Drawing.Point(325, 307);
+            this.LoadDataButton.Name = "LoadDataButton";
+            this.LoadDataButton.Size = new System.Drawing.Size(285, 42);
+            this.LoadDataButton.TabIndex = 3;
+            this.LoadDataButton.Text = "Load Data";
+            this.LoadDataButton.UseVisualStyleBackColor = false;
+            this.LoadDataButton.Click += new System.EventHandler(this.LoadDataButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 355);
+            this.Controls.Add(this.LoadDataButton);
+            this.Controls.Add(this.LoadSchemaButton);
+            this.Controls.Add(this.OrdersGrid);
+            this.Controls.Add(this.CustomersGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CustomersGrid;
+        private System.Windows.Forms.DataGridView OrdersGrid;
+        private System.Windows.Forms.Button LoadSchemaButton;
+        private System.Windows.Forms.Button LoadDataButton;
+    }
+}
+
