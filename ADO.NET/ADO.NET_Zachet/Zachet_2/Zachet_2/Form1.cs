3593baa831b69ec5ebd8a37785816975fca64a09
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2021-02-24 22:03:35 +0300

    Обновление зачётных заданий

---
+++ ADO.NET/ADO.NET_Zachet/Zachet_2/Zachet_2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zachet_2
+{
+    public partial class Form1 : Form
+    {
+        private Panel advancedPanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = adwentureWorksDataSet1.Customers;
+            sqlDataAdapter1.Fill(adwentureWorksDataSet1.Customers);
+        }
+        
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            sqlDataAdapter1.Update(adwentureWorksDataSet1);
+        }
+        
+    }
+}
