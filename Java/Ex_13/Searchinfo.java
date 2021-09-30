3a452c3116d80fb3f7a145e79dcccc83a92b16a2
OlgaLsh <73122567+OlgaLsh@users.noreply.github.com>
2021-09-30 18:49:01 +0300

    ex_13

---
+++ Java/Ex_13/Searchinfo.java
@@ -0,0 +1,39 @@
+
+package ru.lashkevich;
+
+//import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+//@Generated("jsonschema2pojo")
+public class Searchinfo {
+
+    @SerializedName("totalhits")
+    @Expose
+    private Integer totalhits;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Searchinfo() {
+    }
+
+    /**
+     * 
+     * @param totalhits
+     */
+    public Searchinfo(Integer totalhits) {
+        super();
+        this.totalhits = totalhits;
+    }
+
+    public Integer getTotalhits() {
+        return totalhits;
+    }
+
+    public void setTotalhits(Integer totalhits) {
+        this.totalhits = totalhits;
+    }
+
+}
